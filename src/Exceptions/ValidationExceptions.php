595d2e65b52fc04ef9579fd7f250b69ab6246282
RunnerLee <runnerleer@gmail.com>
2017-02-22 02:29:07 +0800

    add exception

---
+++ src/Exceptions/ValidationExceptions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author: RunnerLee
+ * @email: runnerleer@gmail.com
+ * @time: 17-2-21 下午11:15
+ */
+namespace Runner\Validator;
+
+use RuntimeException;
+
+class ValidationExceptions extends RuntimeException
+{
+    
+}
